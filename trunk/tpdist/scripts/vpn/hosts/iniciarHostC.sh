6a08470e5c4ab263887108e7ea8082fe818461ca
karmican <karmican@2dc74635-fd82-4fc3-5960-65eae24a3d15>
2012-12-02 14:54:12 +0000

    Commit de scripts de vpn porque me abrocho coco

--- trunk/tpdist/scripts/vpn/hosts/iniciarHostC.sh
+++
@@ -1,4 +0,0 @@
-
-# $1 -> Ip del servidor
-
-./iniciarCliente.sh $1 1204 10.92.27.6 255.255.255.128
--- trunk/tpdist/scripts/vpn/hosts/start.sh
+++
@@ -1,7 +0,0 @@
-# Params
-# 1 -> ip remoto
-# 2 -> puerto
-# 3 -> ip de topologia
-# 4 -> mascara topologia
-
-openvpn --config ./host.conf --remote $1 --port $2 --ifconfig $3 $4
