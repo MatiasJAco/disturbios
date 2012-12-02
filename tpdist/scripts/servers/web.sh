e433728fa2c08555f3a137bfbc22e7c082ea9868
karmican <karmican@2dc74635-fd82-4fc3-5960-65eae24a3d15>
2012-12-02 22:17:28 +0000

    rutas web

--- tpdist/scripts/servers/web.sh
+++ tpdist/scripts/servers/web.sh
@@ -11,28 +11,59 @@ then
 fi
 
 #Baja el servicio network-manager 
-/etc/init.d/network-manager stop 
+#/etc/init.d/network-manager stop 
 
 #Hace flush de las tablas que contienen los ips
-iptables -F
+#iptables -F
 
 #Borrar todas las direcciones de ip asignada a la interfaz $1
-ip addr flush dev $1
+#ip addr flush dev $1
 
 #H /26
 #Configura la direccion IP de la interfaz $1
 #ifconfig $1:0 up 10.92.27.7 netmask 255.255.255.128
-ifconfig $1 up 192.168.8.71 netmask 255.255.255.192
+#ifconfig $1 up 192.168.8.71 netmask 255.255.255.192
 
 #Habilita IP forwarding (Aparentemente necesario para cuando hay subinterfaces)
 #echo 1 > /proc/sys/net/ipv4/ip_forward
 
-#Default
-route add default gw 192.168.8.67
+#A / 25 
+route add -net 10.92.27.0 netmask 255.255.255.128 gw 192.168.8.67  metric 2
+#B / 24
+route add -net 10.24.3.0 netmask 255.255.255.0 gw 192.168.8.67  metric 2
+#C / 30
+route add -net 10.92.27.128 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+#D / 27
+route add -net 10.24.1.0 netmask 255.255.255.224 gw 192.168.8.67  metric 1
+#E / 28
+route add -net 10.24.1.96 netmask 255.255.255.240 gw 192.168.8.67  metric 1
+#F / 30
+route add -net 10.92.27.132 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+#G / 25
+route add -net 10.24.1.128 netmask 255.255.255.128 gw 192.168.8.67  metric 2
+#I / 24
+route add -net 10.10.5.0 netmask 255.255.255.0 gw 192.168.8.67  metric 2
+#J / 28
+route add -net 10.24.1.112 netmask 255.255.255.240 gw 192.168.8.67  metric 3
+#K / 29
+route add -net 10.92.27.136 netmask 255.255.255.248 gw 192.168.8.67  metric 3
+#L / 27
+route add -net 10.24.1.32 netmask 255.255.255.224 gw 192.168.8.67  metric 2
+#M / 30
+route add -net 172.43.0.64 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+route add -net 172.43.0.68 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+route add -net 172.43.0.76 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+route add -net 172.43.0.80 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+route add -net 172.43.0.84 netmask 255.255.255.252 gw 192.168.8.67  metric 1
+
+#N / 27
+route add -net 10.24.1.64 netmask 255.255.255.224 gw 192.168.8.67  metric 1
+#O / 24
+route add -net 133.43.1.0 netmask 255.255.255.0 gw 192.168.8.67  metric 2
 
 #Configurar el DNS 
 # DNS 2 Rio Negro
-echo "nameserver 10.24.3.5" > /etc/resolv.conf
+#echo "nameserver 10.24.3.5" > /etc/resolv.conf
 
 echo "Server Web Configurado"
 
