9598fa669c5cffc5851e621094dc84cde9acf82f
karmican <karmican@2dc74635-fd82-4fc3-5960-65eae24a3d15>
2012-12-03 13:55:42 +0000

    None | Ruteo de Servers y Quitar Default GW

--- trunk/tpdist/scripts/servers/telnet.sh
+++ trunk/tpdist/scripts/servers/telnet.sh
@@ -4,29 +4,11 @@
 #Script de configuracion del Telnet Srv #
 #########################################
 
-if [ $# -ne 1 ];
-then
-	echo "Debe llamar al script: ./<script>.sh ethX"
-	exit 1
-fi
+# El Telnet server pertence a las Redes G y N
+#G /25 IP 10.24.1.129
+#N /27 IP 10.24.1.65
 
-#Baja el servicio network-manager 
-/etc/init.d/network-manager stop 
-
-#Hace flush de las tablas que contienen los ips
-iptables -F
-
-#Borrar todas las direcciones de ip asignada a la interfaz $1
-ip addr flush dev $1
-
-#G /25
-#Configura la direccion IP de la interfaz $1
-ifconfig $1 up 10.24.1.129 netmask 255.255.255.128
-#N /27
-ifconfig $1:0 up 10.24.1.65 netmask 255.255.255.224
-
-#Habilita IP forwarding (Aparentemente necesario para cuando hay subinterfaces)
-#echo 1 > /proc/sys/net/ipv4/ip_forward
+#Tabla de Ruteo 
 
 #A / 25 
 route add -net 10.92.27.0 netmask 255.255.255.128 gw 10.24.1.66  metric 2
@@ -56,12 +38,10 @@ route add -net 172.43.0.68 netmask 255.255.255.252 gw 10.24.1.66  metric 1
 route add -net 172.43.0.76 netmask 255.255.255.252 gw 10.24.1.66  metric 1
 route add -net 172.43.0.80 netmask 255.255.255.252 gw 10.24.1.66  metric 1
 route add -net 172.43.0.84 netmask 255.255.255.252 gw 10.24.1.66  metric 1
+route add -net 172.43.0.88 netmask 255.255.255.252 gw 10.24.1.66  metric 1
 
 #O / 24
 route add -net 133.43.1.0 netmask 255.255.255.0 gw 10.24.1.67  metric 2
-
-#Default
-route add default gw 10.24.1.66
 
 #Configurar el DNS 
 # DNS 2 Punta Indio
@@ -70,6 +50,6 @@ route add default gw 10.24.1.66
 # DNS Root
 echo "nameserver 10.24.1.133" > /etc/resolv.conf
 
-echo "Server FTP Configurado"
+echo "Server Telnet Configurado"
 
 exit 0
